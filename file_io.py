2a954f10b44b5a9ddd9f649e7e601b8aac2f96a7
Aleksandra <ajieksandra.93@mail.ru>
2024-10-24 23:09:25 +0500

    Добавлен сохранение ссылки в файл upload_history

--- file_io.py
+++ file_io.py
@@ -3,30 +3,52 @@ import requests
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
+import pyperclip
+import json
+import os
+
+from tkinterweb.utilities import download
+
+history_file = 'upload_history.json'
+
+def save_history(filepath, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({'filepath': os.path.basename(filepath), 'download_link': link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f, indent=4)
+
+
+
 
 def get_response():
     try:
-        file=fd.askopenfilename()
-        if file:
-            with open(file, 'rb') as fi:
-                f = {'file': fi}
-                answer_json = requests.post('https://file.io/', files=f)
-                if answer_json.status_code == 200:
-                    link = answer_json.json()['link']
-                    e.delete(0, END)
-                    e.insert(0, link)
+        filepath=fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                file = {'file': f}
+                response = requests.post('https://file.io/', files=file)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0, END)
+                entry.insert(0, link)
+                pyperclip.copy(link)
+                save_history(filepath, link)
+                mb.showinfo('Ссылка', f'Ссылка {link} успешно скопирована в буфер обмена')
     except Exception as ex:
         mb.showerror('Ошибка', f'Произошла ошибка: {ex}')
 
 
 window = Tk()
 window.title('Отправка файлов в file.io')
-window.geometry(f'400x300+{window.winfo_screenwidth()//2-200}+{window.winfo_screenheight()//2-150}')
+window.geometry(f'400x200+{window.winfo_screenwidth()//2-200}+{window.winfo_screenheight()//2-150}')
 
-btn = Button(window, text='Выбрать файл', font=('Arial', 16), command=get_response)
+btn = ttk.Button(window, text='Загрузить файл', command=get_response)
 btn.pack(pady=10)
 
-e = Entry(window, width=30, font=('Arial', 16))
-e.pack(pady=10)
+entry = ttk.Entry(window, width=30)
+entry.pack(pady=10)
 
 window.mainloop()
